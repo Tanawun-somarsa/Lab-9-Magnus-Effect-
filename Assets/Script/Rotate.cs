3b9325eb9fc96b6677dc2f9f82e7bab64a5e9aa0
Tanawun-somarsa <tanawun.soma@bumail.net>
2025-03-20 15:47:47 +0700

    Add torque

--- Assets/Script/Rotate.cs
+++ Assets/Script/Rotate.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Rotate : MonoBehaviour
 {      
     Rigidbody rb;
-    [SerializeField] Vector3 angularV;
+    [SerializeField] Vector3 angularV, torque;
     void Start()
     {
         rb= GetComponent<Rigidbody>();
@@ -14,6 +14,15 @@ public class Rotate : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             rb.angularVelocity = angularV;
+        }
+
+        else if (Input.GetMouseButtonDown(1))
+        {
+            rb.AddTorque(torque);
+        }
+        else if(Input.GetKeyDown(KeyCode.E)) 
+        {
+            rb.angularVelocity = Vector3.zero;
         }
         
     }
